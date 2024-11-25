1cd19d9ae314fc9c0544b8be82004a7e4ef2dbf0
Pedro Fernandes <pednetocn@gmail.com>
2024-11-25 18:36:42 -0300

    first commit

---
+++ lib/ex_mon.ex
@@ -0,0 +1,31 @@
+defmodule ExMon do
+  alias ExMon.{Game, Player}
+  alias ExMon.Game.{Actions, Status}
+
+  def create_player(move_avg, move_heal, move_rnd, name) do
+    Player.build(move_avg, move_heal, move_rnd, name)
+  end
+
+  def start_game(player) do
+    :punch
+    |> create_player(:heal, :kick, "Messi")
+    |> Game.start(player)
+
+    Status.print_round_message()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "realiza cura"
+      move -> Actions.attack(move)
+    end
+  end
+end
